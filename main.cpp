8d9782185a541152e18df696137e14c8532e2f94
mmitkevich <mim@exante.eu>
2018-07-19 17:49:39 +0300

    minimal boost example (WIP)

--- main.cpp
+++ main.cpp
@@ -1,13 +1,46 @@
 #include <memory>
 #include <iostream>
 #include <string>
-#include "src/wallet/api/wallet2_api.h"
-#include "contrib/epee/include/misc_log_ex.h"
-
+#include <boost/thread/thread.hpp>
+#include <boost/thread/mutex.hpp>
 int main(int argc, char ** argv) {
-    Monero::WalletManagerFactory::setLogLevel(Monero::WalletManagerFactory::LogLevel_Max);
-    
-    auto walletManager = std::unique_ptr<Monero::WalletManager>(Monero::WalletManagerFactory::getWalletManager());
+    struct Context {
+        boost::mutex mutex;
+        boost::condition_variable cv;
+        boost::thread thread;
+        std::atomic<bool> should_stop;
 
-    auto wallet = std::unique_ptr<Monero::Wallet>(walletManager->createWallet("test-wallet", "123", "en", Monero::NetworkType::MAINNET));
+        Context() {
+            should_stop = false;
+            thread = boost::thread([this] () {
+                    run();
+                });
+            boost::this_thread::sleep_for(boost::chrono::seconds(1));
+        }
+
+        void run() {
+            boost::mutex::scoped_lock lock(mutex);
+            boost::posix_time::milliseconds wait_for_ms(1);
+            while(!should_stop) {
+                std::cout << "wait\n";
+                cv.timed_wait(lock, wait_for_ms);
+                std::cout << "run\n";
+                boost::this_thread::sleep_for(boost::chrono::milliseconds(300));
+            }
+            std::cout << "done\n";
+        }
+
+        ~Context() {
+            {
+                boost::mutex::scoped_lock lock(mutex);
+                should_stop = true;
+                cv.notify_one();
+                std::cout << "join\n";
+            }
+            thread.join();
+        }
+
+    };
+
+    Context ctx;
 }
